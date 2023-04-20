9e914c092438eaa5fa77a7b219a38c3b4c39f331
Manoela Urias <maanoelaurias@gmail.com~>
2023-04-19 22:15:58 -0300

    modificações

--- app.js
+++ app.js
@@ -5,4 +5,22 @@ function changePage(page) {
         document.querySelector("body").innerHTML = data;
       });
   }
-  
+
+const birthday = new Date('2001-01-24'); // Insira aqui sua data de nascimento
+const ageElement = document.getElementById('age');
+
+function updateAge() {
+  const age = calculateAge(birthday);
+  const ageSpan = document.createElement('span');
+  ageSpan.classList.add('destaques');
+  ageSpan.textContent = age;
+  ageElement.appendChild(ageSpan); // Adiciona o span como filho do elemento age
+}
+
+function calculateAge(birthday) {
+  const ageDiffMs = Date.now() - birthday.getTime();
+  const ageDate = new Date(ageDiffMs);
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+document.addEventListener('DOMContentLoaded', updateAge); // Adicione essa linha para chamar updateAge() no carregamento da página inicial
