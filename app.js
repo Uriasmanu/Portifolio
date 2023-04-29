ff54c9127b21a273fc9b795d2ef8e0ce6a5831a5
Manoela Urias <maanoelaurias@gmail.com~>
2023-04-29 11:21:40 -0300

    correção

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function changePage(page) {
-  fetch(page)
-    .then(response => response.text())
-    .then(data => {
-      document.querySelector("body").innerHTML = data;
-    });
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      document.querySelector("#conteudo").innerHTML = this.responseText;
+    }
+  };
+  xhr.open("GET", page, true);
+  xhr.send();
 }
